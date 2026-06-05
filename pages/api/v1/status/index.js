6b3ba53fc38c6923e00f124606efee79faa3170a
Artarq2 <arthmliveira@gmail.com>
2026-06-05 14:55:42 -0300

    add o arquivo

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;  "); // a variavel result, recebe o resultado da consulta feita ao banco de dados, ou seja, ela vai esperar o banco de dados responder com o resultado da consulta, antes de continuar executando o próximo código.
