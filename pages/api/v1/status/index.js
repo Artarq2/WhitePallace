f273a436204f325bacad1bac144f664e7b8b2a1f
Artarq2 <arthmliveira@gmail.com>
2026-06-08 17:02:17 -0300

    Adiciona toda as modificações e finaliza endpoit

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,55 @@
-import database from "infra/database";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;  "); // a variavel result, recebe o resultado da consulta feita ao banco de dados, ou seja, ela vai esperar o banco de dados responder com o resultado da consulta, antes de continuar executando o próximo código.
-  console.log(result.rows); // imprime o resultado da consulta feita ao banco de dados, ou seja, ela vai imprimir o resultado da consulta feita ao banco de dados, que é um objeto com as propriedades rows, rowCount, etc.
-  response.status(200).json({ chave: "tudo conforme o esperado" });
+  const updateAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+  const databaseName = process.env.POSTGRES_DB;
+  const conexaoativaResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+    //  "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  });
+  const conexaoativa = conexaoativaResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        database_version: databaseVersion,
+        max_connections: maxConnections,
+        conexaoativa: conexaoativa,
+      },
+    },
+  });
 }
 
 export default status;
+
+/*
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const updateAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+  const conexaoativaResult = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity;",
+  );
+  const conexaoativa = parseInt(conexaoativaResult.rows[0].count);
+
+  response.status(200).json({
+    update_at: updateAt,
+    database_version: databaseVersion,
+    max_connections: maxConnections,
+    conexaoativa: conexaoativa,
+           
+}
+
+export default status;
+
+*/
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,62 @@
 test("GET to api/v1/status should return 200", async () => {
+  // Faz uma requisição para a rota e guarda a resposta
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(200); // Verifica se o servidor respondeu com status 200 (tudo certo)
+
+  const responseBody = await response.json(); // Converte o conteúdo da resposta de JSON para objeto JavaScript
+  expect(responseBody.update_at).toBeDefined(); // Verifica se o campo update_at existe na resposta
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString(); // Pega o update_at recebido e converte para o formato de data padrão ISO
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.database_version).toBeDefined(); // Verifica se o campo database_version existe na resposta
+  const parsedDatabaseVersion =
+    responseBody.dependencies.database.database_version; // Pega o database_version recebido
+  expect(typeof parsedDatabaseVersion).toBe("string"); // Verifica se o database_version é uma string
+  expect(parsedDatabaseVersion.length).toBeGreaterThan(0); // Verifica se o database_version não é uma string vazia
+  console.log("Database version:", parsedDatabaseVersion); // Imprime a versão do banco de dados no console para verificar o valor retornado
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined(); // Verifica se o campo max_connections existe na resposta
+  const parsedMaxConnections =
+    responseBody.dependencies.database.max_connections;
+  expect(typeof parsedMaxConnections).toBe("number"); // Verifica se o max_connections é um número
+  expect(parsedMaxConnections).toBeGreaterThan(0); // Verifica se o max_connections é um número positivo
+  console.log("Max connections:", parsedMaxConnections); // Imprime o max_connections no console para verificar o valor retornado
+
+  expect(responseBody.dependencies.database.conexaoativa).toBeDefined();
+  const parsedConexaoAtiva = responseBody.dependencies.database.conexaoativa;
+  expect(typeof parsedConexaoAtiva).toBe("number");
+  expect(parsedConexaoAtiva).toEqual(1);
+  console.log("Conexão ativa:", parsedConexaoAtiva);
 });
+
+/*
+test("GET to api/v1/status should return 200", async () => {
+  // Faz uma requisição para a rota e guarda a resposta
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200); // Verifica se o servidor respondeu com status 200 (tudo certo)
+
+  const responseBody = await response.json(); // Converte o conteúdo da resposta de JSON para objeto JavaScript
+  expect(responseBody.update_at).toBeDefined(); // Verifica se o campo update_at existe na resposta
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString(); // Pega o update_at recebido e converte para o formato de data padrão ISO
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.database_version).toBeDefined(); // Verifica se o campo database_version existe na resposta
+  const parsedDatabaseVersion = responseBody.database_version; // Pega o database_version recebido
+  expect(typeof parsedDatabaseVersion).toBe("string"); // Verifica se o database_version é uma string
+  expect(parsedDatabaseVersion.length).toBeGreaterThan(0); // Verifica se o database_version não é uma string vazia
+  console.log("Database version:", parsedDatabaseVersion); // Imprime a versão do banco de dados no console para verificar o valor retornado
+
+  expect(responseBody.max_connections).toBeDefined(); // Verifica se o campo max_connections existe na resposta
+  const parsedMaxConnections = responseBody.max_connections;
+  expect(typeof parsedMaxConnections).toBe("number"); // Verifica se o max_connections é um número
+  expect(parsedMaxConnections).toBeGreaterThan(0); // Verifica se o max_connections é um número positivo
+  console.log("Max connections:", parsedMaxConnections); // Imprime o max_connections no console para verificar o valor retornado
+
+  expect(responseBody.conexaoativa).toBeDefined(); 
+  const parsedConexaoAtiva = responseBody.conexaoativa;
+  expect(typeof parsedConexaoAtiva).toBe("number"); 
+  expect(parsedConexaoAtiva).toBeGreaterThanOrEqual(0); 
+  console.log("Conexão ativa:", parsedConexaoAtiva); 
+*/
