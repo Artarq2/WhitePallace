72b9d6deee75e0815b3cb176265659e9cdb76f59
Artarq2 <arthmliveira@gmail.com>
2026-06-09 16:22:01 -0300

    add `ssl to git for Neon request`

--- infra/database.js
+++ infra/database.js
@@ -10,13 +10,14 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER, // o usuário do banco de dados, ou seja, o nome de usuário que tem acesso ao banco de dados. No caso, postgres é o usuário padrão do banco de dados postgres.
     database: process.env.POSTGRES_DB, // o nome do banco de dados, ou seja, o nome do banco de dados que você quer conectar. No caso, postgres é o nome do banco de dados padrão do postgres.
     password: process.env.POSTGRES_PASSWORD, // a senha do banco de dados, ou seja, a senha do usuário que tem acesso ao banco de dados. No caso, localpostgres é a senha que eu configurei para o usuário postgres no meu banco de dados.
+    ssl: { rejectUnauthorized: false },
   }); // a variavel client , recebe uma nova instancia da classe client, ou seja, ela é um objeto que tem os metodos para conectar com o banco de dados, fazer consultas, etc.
   console.log("Credencias do Postgres:", {
-    host: process.env.POSTGRES_HOST, // o host do banco de dados, ou seja, o endereço onde o banco de dados está rodando. No caso, localhost significa que o banco de dados está rodando na mesma máquina onde o código está sendo executado.
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER, // o usuário do banco de dados, ou seja, o nome de usuário que tem acesso ao banco de dados. No caso, postgres é o usuário padrão do banco de dados postgres.
-    database: process.env.POSTGRES_DB, // o nome do banco de dados, ou seja, o nome do banco de dados que você quer conectar. No caso, postgres é o nome do banco de dados padrão do postgres.
-    password: process.env.POSTGRES_PASSWORD, // a senha do banco de dados, ou seja, a senha do usuário que tem acesso ao banco de dados. No caso, localpostgres é a senha que eu configurei para o usuário postgres no meu banco de dados.
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   try {
     await client.connect(); // espera a conexão com o banco de dados ser estabelecida, ou seja, ela vai esperar o banco de dados responder que a conexão foi estabelecida, antes de continuar executando o próximo código.
