2ad1303eab3398c90d7dc03ffbbf516b84a2bfd9
Artarq2 <arthmliveira@gmail.com>
2026-08-17 17:27:22 -0300

    test: rename all descriptions to use new pattern

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -6,15 +6,19 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("GET to api/v1/migrations should return 200", async () => {
-  // Faz uma requisição para a rota e guarda a resposta
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200); // Verifica se o servidor respondeu com status 200 (tudo certo)
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      // Faz uma requisição para a rota e guarda a resposta
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200); // Verifica se o servidor respondeu com status 200 (tudo certo)
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-  console.log(responseBody);
-  console.log(process.env.POSTGRES_HOST);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+      console.log(responseBody);
+      console.log(process.env.POSTGRES_HOST);
+    });
+  });
 });
